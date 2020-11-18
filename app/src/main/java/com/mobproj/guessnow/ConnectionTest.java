21d86452acd11a9ff6ef4d78f7381455269e9ed0
Rafli <raflisatriandi@outlook.co.id>
2020-11-18 23:29:03 +0700

    recycler view support added
    recycler view support added

--- app/src/main/java/com/mobproj/guessnow/ConnectionTest.java
+++ app/src/main/java/com/mobproj/guessnow/ConnectionTest.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
@@ -18,11 +19,35 @@ public class ConnectionTest extends AppCompatActivity {
         } catch(URISyntaxException e){}
     }
 
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    String username;
+                    String message;
+                    try {
+                        username = data.getString("username");
+                        message = data.getString("message");
+                    } catch (JSONException e) {
+                        return;
+                    }
+
+                    // add the message to view
+                    addMessage(username, message);
+                }
+            });
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection_test);
+
+        msocket.on("new message", onNewMessage);
         msocket.connect();
     }
 }
