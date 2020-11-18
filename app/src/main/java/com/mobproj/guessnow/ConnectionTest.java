f2c57eed87b820b800b1e39a62a6ebdaa971985f
Rafli <raflisatriandi@outlook.co.id>
2020-11-19 01:48:14 +0700

    Update ConnectionTest.java

--- app/src/main/java/com/mobproj/guessnow/ConnectionTest.java
+++ app/src/main/java/com/mobproj/guessnow/ConnectionTest.java
@@ -8,6 +8,9 @@ import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.net.URISyntaxException;
 
 public class ConnectionTest extends AppCompatActivity {
@@ -15,39 +18,39 @@ public class ConnectionTest extends AppCompatActivity {
     private Socket msocket;
     {
         try{
-            msocket = IO.socket("http://chat.socket.io");
+            msocket = IO.socket("http://localhost:3000");
         } catch(URISyntaxException e){}
     }
 
-    private Emitter.Listener onNewMessage = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    JSONObject data = (JSONObject) args[0];
-                    String username;
-                    String message;
-                    try {
-                        username = data.getString("username");
-                        message = data.getString("message");
-                    } catch (JSONException e) {
-                        return;
-                    }
-
-                    // add the message to view
-                    addMessage(username, message);
-                }
-            });
+    private Emitter.Listener onNewMessage = args -> this.runOnUiThread(() -> {
+        JSONObject data = (JSONObject) args[0];
+        String username;
+        String message;
+        try {
+            username = data.getString("username");
+            message = data.getString("message");
+        } catch (JSONException e) {
+            return;
         }
-    };
+
+        // add the message to view
+//        addMessage(username, message);
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection_test);
 
-        msocket.on("new message", onNewMessage);
+        msocket.on("123", onNewMessage);
         msocket.connect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        msocket.disconnect();
+        msocket.off("123", onNewMessage);
     }
 }
