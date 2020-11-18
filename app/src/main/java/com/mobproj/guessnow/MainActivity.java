533b84010db73c3b40314a2033be831b6b0f6fc6
Rafli <raflisatriandi@outlook.co.id>
2020-11-18 21:46:11 +0700

    Some changes
    Some changes

--- app/src/main/java/com/mobproj/guessnow/MainActivity.java
+++ app/src/main/java/com/mobproj/guessnow/MainActivity.java
@@ -1,8 +1,11 @@
 package com.mobproj.guessnow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_navigation);
+        bottomNavView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = item -> {
+
+        Fragment selectedFragment=null;
+        switch (item.getItemId()){
+            case R.id.home:
+                selectedFragment= new HomeFragment();
+                break;
+
+            case R.id.user:
+                selectedFragment= new UserFragment();
+                break;
+
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+        return true;
+    };
 }
