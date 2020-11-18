533fe549c066d39898c40e849de417d916c9792f
Rafli <raflisatriandi@outlook.co.id>
2020-11-18 22:47:52 +0700

    socket io connection
    socket io connection

--- app/src/main/java/com/mobproj/guessnow/ConnectionTest.java
+++ app/src/main/java/com/mobproj/guessnow/ConnectionTest.java
@@ -23,5 +23,6 @@ public class ConnectionTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection_test);
+        msocket.connect();
     }
 }
