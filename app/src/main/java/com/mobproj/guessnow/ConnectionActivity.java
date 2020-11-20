a2abf31bb2434a4fd1b433f41be197211d0f7e48
Rafli <raflisatriandi@outlook.co.id>
2020-11-20 21:45:41 +0700

    Connection Success
    Connection Success

---
+++ app/src/main/java/com/mobproj/guessnow/ConnectionActivity.java
@@ -0,0 +1,59 @@
+package com.mobproj.guessnow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.engineio.client.Transport;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Manager;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+public class ConnectionActivity extends AppCompatActivity {
+
+    private Socket socket;
+    private EditText inputMsgView;
+    private String inputMsg, URL = "http://192.168.18.163:3000", room = "789";
+
+    private Emitter.Listener newMsg = args -> {
+        this.runOnUiThread(() -> {
+            JSONObject data = (JSONObject) args[0];
+        });
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection_test);
+
+        inputMsgView = (EditText) findViewById(R.id.msg_input);
+        try {
+            socket = IO.socket("http://192.168.18.163:3000");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        socket.on(room, newMsg);
+        socket.connect();
+
+    }
+
+    public void sendBtn(View view) {
+        inputMsg = inputMsgView.getText().toString();
+        if(TextUtils.isEmpty(inputMsg))
+            return;
+
+        inputMsgView.getText().clear();
+        socket.emit(room, inputMsg);
+    }
+}
---
+++ app/src/main/java/com/mobproj/guessnow/SocketIO_Connection.java
@@ -0,0 +1,23 @@
+package com.mobproj.guessnow;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
+
+public class SocketIO_Connection {
+
+    private Socket socket;
+
+    public void connect(String URL){
+        try {
+            socket = IO.socket(URL);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        socket.connect();
+    }
+
+    public void emit(){
+    }
+}
