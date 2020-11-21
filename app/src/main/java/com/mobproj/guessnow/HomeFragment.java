6b1755c7ccfb1ed02f08a944b4f02773bb990f1f
as13af <alishidqie.asaf@gmail.com>
2020-11-22 05:53:32 +0700

    Yowasapyo
    Ganti lagi di HomeFragment dan Fragment_home

--- app/src/main/java/com/mobproj/guessnow/HomeFragment.java
+++ app/src/main/java/com/mobproj/guessnow/HomeFragment.java
@@ -5,35 +5,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment{
+    EditText etUniqueCode;
     private Button btn_createroom;
-    public static final int LENGTH_SHORT;
+    public static final int LENGTH_SHORT = 0;
+
+
+
 
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     { View view = inflater.inflate(R.layout.fragment_home, container);
-    init(view);
-    btn_createroom = view.findViewById(R.id.btn_createroom);
+    init(View);
+
     btn_createroom.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            btn_createroom(getActivity(),"", btn_createroom.LENGTH_SHORT).show();
-
-
+            Toast.makeText(getActivity(), "", Toast.LENGTH_SHORT).show();
         }
     });
+
 
         return view;
 
     }
-    private void init(View view){
+
+    private void init(View view) {
         btn_createroom = view.findViewById(R.id.btn_createroom);
     }
+
 }
