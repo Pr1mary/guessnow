b73651dc8dc8ae0f707e521b67246592a1d63283
as13af <alishidqie.asaf@gmail.com>
2020-11-22 03:08:29 +0700

    HomeFragment btn_createroom
    penambahan btn_createroom

--- app/src/main/java/com/mobproj/guessnow/HomeFragment.java
+++ app/src/main/java/com/mobproj/guessnow/HomeFragment.java
@@ -10,27 +10,30 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment extends Fragment implements View.OnClickListener{
+public class HomeFragment extends Fragment{
     private Button btn_createroom;
+    public static final int LENGTH_SHORT;
 
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
-        btn_createroom = (Button) findViewById(R.id.btn_createroom);
-        btn_createroom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                open
-
-            }
-        });
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    { View view = inflater.inflate(R.layout.fragment_home, container);
+    init(view);
+    btn_createroom = view.findViewById(R.id.btn_createroom);
+    btn_createroom.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            btn_createroom(getActivity(),"", btn_createroom.LENGTH_SHORT).show();
 
 
+        }
+    });
+
+        return view;
 
     }
-    public void ->Masukin nama activity di HomeFragment(){
-
+    private void init(View view){
+        btn_createroom = view.findViewById(R.id.btn_createroom);
     }
 }
