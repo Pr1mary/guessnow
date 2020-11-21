876067f64d25a5b664546d8cfbc65210e633bc00
as13af <alishidqie.asaf@gmail.com>
2020-11-22 02:24:39 +0700

    Changes
    Sama dengan sebelumnya cuma nambah create_room

---
+++ app/src/main/java/com/mobproj/guessnow/Create_room.java
@@ -0,0 +1,15 @@
+package com.mobproj.guessnow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Create_room extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_room);
+    }
+}
--- app/src/main/java/com/mobproj/guessnow/HomeFragment.java
+++ app/src/main/java/com/mobproj/guessnow/HomeFragment.java
@@ -10,15 +10,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment extends Fragment {
-    private Button createroom;
+public class HomeFragment extends Fragment implements View.OnClickListener{
+    private Button btn_createroom;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
-        createroom = (Button) findViewById(R.id.createroom);
-        createroom.setOnClickListener(new View.OnClickListener() {
+        btn_createroom = (Button) findViewById(R.id.btn_createroom);
+        btn_createroom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 open
