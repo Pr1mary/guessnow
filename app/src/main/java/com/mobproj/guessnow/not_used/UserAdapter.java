a431abfa15dc8f4c264037c4c56d4a222db2c70b
Rafli <raflisatriandi@outlook.co.id>
2020-11-27 22:55:33 +0700

    activity game update
    activity game update

--- app/src/main/java/com/mobproj/guessnow/not_used/UserAdapter.java
+++ app/src/main/java/com/mobproj/guessnow/not_used/UserAdapter.java
@@ -1,70 +1,70 @@
-package com.mobproj.guessnow.not_used;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.mobproj.guessnow.R;
-
-import java.util.ArrayList;
-
-public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
-
-    private ArrayList<User> list;
-
-    private clikListener listener;
-
-    public UserAdapter(ArrayList<User> list, clikListener listener){
-        this.listener = listener;
-        this.list = list;
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
-        private TextView Nama;
-        private TextView Score;
-
-
-        public ViewHolder(final View view){
-            super(view);
-            Nama = view.findViewById(R.id.nama);
-            Score = view.findViewById(R.id.score);
-            view.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View view) {
-            listener.onClik(view, getAdapterPosition());
-        }
-    }
-
-
-    @NonNull
-    @Override
-    public UserAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemV = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_game_info, parent, false);
-        return new ViewHolder(itemV);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull UserAdapter.ViewHolder holder, int position) {
-        String nama = list.get(position).getNama();
-        String score = list.get(position).getScore();
-
-        holder.Nama.setText(nama);
-        holder.Score.setText(score);
-    }
-
-    @Override
-    public int getItemCount() {
-        return list.size();
-    }
-
-    public interface clikListener{
-        void onClik(View v, int position);
-    }
-}
-
+//package com.mobproj.guessnow.not_used;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import com.mobproj.guessnow.R;
+//
+//import java.util.ArrayList;
+//
+//public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+//
+//    private ArrayList<User> list;
+//
+//    private clikListener listener;
+//
+//    public UserAdapter(ArrayList<User> list, clikListener listener){
+//        this.listener = listener;
+//        this.list = list;
+//    }
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
+//        private TextView Nama;
+//        private TextView Score;
+//
+//
+//        public ViewHolder(final View view){
+//            super(view);
+////            Nama = view.findViewById(R.id.nama);
+////            Score = view.findViewById(R.id.score);
+//            view.setOnClickListener(this);
+//        }
+//
+//        @Override
+//        public void onClick(View view) {
+//            listener.onClik(view, getAdapterPosition());
+//        }
+//    }
+//
+//
+//    @NonNull
+//    @Override
+//    public UserAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View itemV = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_game_info, parent, false);
+//        return new ViewHolder(itemV);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull UserAdapter.ViewHolder holder, int position) {
+//        String nama = list.get(position).getNama();
+//        String score = list.get(position).getScore();
+//
+//        holder.Nama.setText(nama);
+//        holder.Score.setText(score);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return list.size();
+//    }
+//
+//    public interface clikListener{
+//        void onClik(View v, int position);
+//    }
+//}
+//
