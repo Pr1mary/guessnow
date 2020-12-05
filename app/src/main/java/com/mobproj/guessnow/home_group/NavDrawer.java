3a99780276afe97cecfd1587adb91a34e28632b7
as13af <alishidqie.asaf@gmail.com>
2020-12-05 18:00:23 +0700

    NavDrawerUpdate

--- app/src/main/java/com/mobproj/guessnow/home_group/NavDrawer.java
+++ app/src/main/java/com/mobproj/guessnow/home_group/NavDrawer.java
@@ -1,14 +1,41 @@
 package com.mobproj.guessnow.home_group;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+import com.mobproj.guessnow.R;
 
 public class NavDrawer extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
+        drawerLayout=findViewById(R.id.toolBar);
+        toolbar=findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.restart,R.string.exit);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                
+            }
+        });
+
+
     }
 }
