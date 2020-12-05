7611f810e0d889b46b08629b5ab99a1da0009397
Rafli <raflisatriandi@outlook.co.id>
2020-12-05 18:53:46 +0700

    some changes
    some changes

--- app/src/main/java/com/mobproj/guessnow/home_group/NavDrawer.java
+++ app/src/main/java/com/mobproj/guessnow/home_group/NavDrawer.java
@@ -22,29 +22,25 @@ public class NavDrawer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
-        drawerLayout=findViewById(R.id.toolBar);
+        drawerLayout = new DrawerLayout(this);
         toolbar=findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
         toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.restart,R.string.exit);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView=findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id=item.getItemId();
-                switch (id)
-                {
-                    case R.id.restart:
-                    break;
-                    case R.id.exit:
-                    break;
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id=item.getItemId();
+            switch (id)
+            {
+                case R.id.restart:
+                break;
+                case R.id.exit:
+                break;
 
-                }
-                return true;
             }
+            return true;
         });
-
 
     }
 }
